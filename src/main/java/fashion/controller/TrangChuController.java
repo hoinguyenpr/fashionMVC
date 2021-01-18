eb00472f74ca516838b863c79bb4387954d909b5
hoinguyenpr <hoinguyenpr.97@gmail.com>
2021-01-19 00:32:07 +0700

    test

---
+++ src/main/java/fashion/controller/TrangChuController.java
@@ -0,0 +1,26 @@
+package fashion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class TrangChuController {
+	@RequestMapping("/")
+	public String ViewTrangChu() {
+		return "trangchu";
+	}
+	
+	@RequestMapping("/login")
+	@ResponseBody
+	public String Login() {
+		return "Login";
+	}
+	
+	@RequestMapping("/register")
+	@ResponseBody
+	public String Register() {
+		return "Register AAAAAAAAAA";
+	}
+}
